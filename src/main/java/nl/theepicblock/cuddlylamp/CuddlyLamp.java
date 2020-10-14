2eac8a8e6e2066f92c1c43cd3efec3b7ba934ce0
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-10-14 22:05:19 +0200

    init repo

---
+++ src/main/java/nl/theepicblock/cuddlylamp/CuddlyLamp.java
@@ -0,0 +1,10 @@
+package nl.theepicblock.cuddlylamp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CuddlyLamp implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
